d35ddc42d0db3dc854bb2c7af71555504674d62b
Ed Rogers <f5d2h4eeyw@snkmail.com>
2015-12-09 13:27:00 -0600

    Added email sending function and diff parsing function

--- CreateNewNeighborsList.py
+++ CreateNewNeighborsList.py
@@ -9,6 +9,32 @@ from datetime import datetime, date
 import config
 from difflib import unified_diff
 
+def send_email(user, pwd, recipient, subject, body):                                                                            
+    import smtplib                                                                                                              
+                                                                                                                                
+    gmail_user = user                                                                                                           
+    gmail_pwd = pwd                                                                                                             
+    FROM = user                                                                                                                 
+    TO = recipient if type(recipient) is list else [recipient]                                                                
+    SUBJECT = subject                                                                                                           
+    TEXT = body                                                                                                                 
+                                                                                                                                
+    # Prepare actual message                                                                                                    
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s                                                                           
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)                                                                                  
+    try:                                                                     
+        # SMTP_SSL Example
+        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server_ssl.ehlo() # optional, called by login()
+        server_ssl.login(gmail_user, gmail_pwd)
+        # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
+        server_ssl.sendmail(FROM, TO, message)
+        #server_ssl.quit()
+        server_ssl.close()
+        print 'successfully sent the mail'
+    except:                                                                  
+        print "failed to send mail"                                                                                             
+
 #First, download all the parcels in Ward 79
 httpRequestURL="https://data.cityofmadison.com/resource/u7ns-6d4x.csv?ward=79&$limit=5000"
 headers={'X-App-Token': config.cityOfMadisonToken}
@@ -94,6 +120,12 @@ betweenSemicolons=re.compile(";[^;]+;")
 
 # Now compare the two most recent Owner Listings for any changes:
 ownerListingFiles = sorted(glob.glob(dirName+"/OwnerListing_*.csv"))
+emailBodyLines = []
+emailBodyLines.append("Hello,")
+emailBodyLines.append("")
+emailBodyLines.append("This is the automatically generated email to notify of changes to parcel listings on the City of Madison website.")
+emailBodyLines.append("")
+
 if len(ownerListingFiles) > 1 :
     previousFileName = ownerListingFiles[-2]
     currentFileName = ownerListingFiles[-1]
@@ -112,32 +144,79 @@ if len(ownerListingFiles) > 1 :
     prevLines = [line[1:] for line in lines if line[0] == '-']
     currLines = [line[1:] for line in lines if line[0] == '+']
 
-    parcelNums = []
-    for prevLine in prevLines :
-        prevParcelNum = parcelNumRE.match(prevLine).group(0)
-        parcelNums.append(prevParcelNum)
-    for currLine in currLines :
-        currParcelNum = parcelNumRE.match(currLine).group(0)
-        parcelNums.append(currParcelNum)
-    parcelNums = sorted(set(parcelNums))
-                
+    parcelsChanged = []
+    parcelsRemoved = []
+    parcelsAdded   = []
     for prevLine in prevLines :
         prevParcelNum = parcelNumRE.match(prevLine).group(0)
         prevAddress = betweenSemicolons.search(prevLine).group(0).strip(';')
         prevOwner = lastCSVEntryRE.search(prevLine).group(0).rstrip('\n')
-        currAddress = None;
-        currOwner = None;
+        matchedThisParcelYet = False
         for currLine in currLines :
             currParcelNum = parcelNumRE.match(currLine).group(0)
-            if currParcelNum == prevParcelNum :
-                currAddress = betweenSemicolons.search(currLine).group(0).strip(';')
-                currOwner = lastCSVEntryRE.search(currLine).group(0).rstrip('\n')
-        if currOwner != None and currAddress != None :
-            print "{} / {} has changed to {} / {}".format(prevOwner,prevAddress,currOwner,currAddress)
-        else :
-            print "Could not find updated information for {} {} ({})".format(prevOwner, prevAddress, prevParcelNum)
+            currAddress = betweenSemicolons.search(currLine).group(0).strip(';')
+            currOwner = lastCSVEntryRE.search(currLine).group(0).rstrip('\n')
+            if prevParcelNum == currParcelNum :
+                matchedThisParcelYet = True
+                parcelsChanged.append([prevParcelNum,prevAddress,prevOwner,currAddress,currOwner])
+                break
+        if matchedThisParcelYet == False :
+            parcelsRemoved.append([prevParcelNum,prevAddress,prevOwner])
+    for currLine in currLines :
+        currParcelNum = parcelNumRE.match(currLine).group(0)
+        currAddress = betweenSemicolons.search(currLine).group(0).strip(';')
+        currOwner = lastCSVEntryRE.search(currLine).group(0).rstrip('\n')
+        matchedThisParcelYet = False
+        for prevLine in prevLines :
+            prevParcelNum = parcelNumRE.match(prevLine).group(0)
+            if prevParcelNum == currParcelNum :
+                matchedThisParcelYet = True
+        if matchedThisParcelYet == False :
+            parcelsAdded.append([currParcelNum,currAddress,currOwner])
+
+    if len(parcelsChanged) > 0 :
+        emailBodyLines.append("")
+        emailBodyLines.append("The following {} parcel(s) have changed:".format(len(parcelsChanged)))
+        emailBodyLines.append("")
+        for pC in parcelsChanged :
+            emailBodyLines.append("Parcel #: {}".format(pC[0]))
+            emailBodyLines.append("Owner:   {} --> {}".format(pC[2],pC[4]))
+            emailBodyLines.append("Address: {} --> {}".format(pC[1],pC[3]))
+            emailBodyLines.append("")
+    if len(parcelsRemoved) > 0 :
+        emailBodyLines.append("")
+        emailBodyLines.append("The following {} parcel(s) have been removed from the city listing:".format(len(parcelsRemoved)))
+        emailBodyLines.append("")
+        for pC in parcelsRemoved :
+            emailBodyLines.append("Parcel #: {}".format(pC[0]))
+            emailBodyLines.append("Owner:   {}".format(pC[2]))
+            emailBodyLines.append("Address: {}".format(pC[1]))
+            emailBodyLines.append("")
+    if len(parcelsAdded) > 0 :
+        emailBodyLines.append("")
+        emailBodyLines.append("The following {} parcel(s) have been added to the city listing:".format(len(parcelsAdded)))
+        emailBodyLines.append("")
+        for pC in parcelsAdded :
+            emailBodyLines.append("Parcel #: {}".format(pC[0]))
+            emailBodyLines.append("Owner:   {}".format(pC[2]))
+            emailBodyLines.append("Address: {}".format(pC[1]))
+            emailBodyLines.append("")
 
     previousFile.close()
     currentFile.close()
+
+    if len(parcelsChanged)+len(parcelsAdded)+len(parcelsRemoved) > 0 :
+        emailBodyLines.append("This email shows updates to the City of Madison Parcel Listings that occurred between {} and {}.".format(prevDate, currDate))
+        emailBodyLines.append("")
+        subjectLine = "Parcel Listing Updates for {dt:%B} {dt:%Y}".format(dt=datetime.now())
+        emailBody = "\r\n".join(emailBodyLines)
+        print subjectLine
+        print emailBody
+        send_email(config.gmailAddress,
+                   config.gmailPassword,
+                   config.targetAddress,
+                   subjectLine,
+                   emailBody)
+    
 
 quit()
