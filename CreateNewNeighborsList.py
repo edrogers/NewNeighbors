2edaa9dd8368a8dd5874b243aa0ecfa35daf6544
Ed Rogers <f5d2h4eeyw@snkmail.com>
2015-12-09 14:06:19 -0600

    Uncommented code to restore full functionality. Ready for first full test.

--- CreateNewNeighborsList.py
+++ CreateNewNeighborsList.py
@@ -41,78 +41,78 @@ headers={'X-App-Token': config.cityOfMadisonToken}
 
 dirName=os.path.dirname(os.path.realpath(__file__))
 
-# r=requests.get(httpRequestURL,headers=headers)
-# filename = "Assessor_Property_Information.csv"
-# try:
-#     os.remove(filename)
-# except OSError:
-#     pass
-# fileout = open(filename,'wb')
-# fileout.write(r.content)
-# fileout.close()
+r=requests.get(httpRequestURL,headers=headers)
+filename = "Assessor_Property_Information.csv"
+try:
+    os.remove(filename)
+except OSError:
+    pass
+fileout = open(filename,'wb')
+fileout.write(r.content)
+fileout.close()
 
-# logfilename = "errorlog.txt"
+logfilename = "errorlog.txt"
 
-# now = datetime.now()
-# csvfilename = "OwnerListing_{dt:%Y%m%d}.csv".format(dt=datetime.now())
+now = datetime.now()
+csvfilename = "OwnerListing_{dt:%Y%m%d}.csv".format(dt=datetime.now())
 
-# try:
-#     os.remove(csvfilename)
-# except OSError:
-#     pass
+try:
+    os.remove(csvfilename)
+except OSError:
+    pass
 
-# #Next, use the parcel numbers & the city website to find the owners' names
+#Next, use the parcel numbers & the city website to find the owners' names
 parcelNumRE=re.compile("[0-9]{12}")
-# parcelAndAddressRE=re.compile("[0-9]{12},[^,]+")
-# for line in open(filename) :
-#     parcelAndAddressObject = parcelAndAddressRE.match(line)
-#     if parcelAndAddressObject != None:
-#         parcelAndAddress = parcelAndAddressObject.group(0).replace(',',';')
-#         parcelNum = parcelNumRE.match(parcelAndAddress).group(0)
+parcelAndAddressRE=re.compile("[0-9]{12},[^,]+")
+for line in open(filename) :
+    parcelAndAddressObject = parcelAndAddressRE.match(line)
+    if parcelAndAddressObject != None:
+        parcelAndAddress = parcelAndAddressObject.group(0).replace(',',';')
+        parcelNum = parcelNumRE.match(parcelAndAddress).group(0)
 
-#         # Download by HTTP a parcel lookup from the city
-#         success = False
-#         attempts = 0
-#         while not success and attempts < 3 :
-#             try :
-#                 resp = requests.get("http://www.cityofmadison.com/assessor/property/propertydata.cfm?ParcelN={}".format(parcelNum))
-#                 success = True
-#             except requests.exceptions.RequestException as e :
-#                 attempts += 1
-#                 logfile = open(logfilename,'a')
-#                 if attempts == 3 :
-#                     className = "ERROR:"
-#                 else:
-#                     sleep(60)
-#                     className = "warn:"
-#                 logfile.write("{} {dt:%c}; Parcel# {}; Attempt#: {}; {}: {}\n".format(className,parcelNum,attempts,type(e),e,dt=datetime.now()))
-#                 logfile.close()
+        # Download by HTTP a parcel lookup from the city
+        success = False
+        attempts = 0
+        while not success and attempts < 3 :
+            try :
+                resp = requests.get("http://www.cityofmadison.com/assessor/property/propertydata.cfm?ParcelN={}".format(parcelNum))
+                success = True
+            except requests.exceptions.RequestException as e :
+                attempts += 1
+                logfile = open(logfilename,'a')
+                if attempts == 3 :
+                    className = "ERROR:"
+                else:
+                    sleep(60)
+                    className = "warn:"
+                logfile.write("{} {dt:%c}; Parcel# {}; Attempt#: {}; {}: {}\n".format(className,parcelNum,attempts,type(e),e,dt=datetime.now()))
+                logfile.close()
 
-#         # Save that content to a local file
-#         filename = "Madison_Parcel_{}.html".format(parcelNum)
-#         fileout = open(filename,'wb')
-#         fileout.write(resp.content)
-#         fileout.close()
-#         # Now open that file for reading
-#         filein = open(filename,'r')
-#         lines = filein.readlines()
-#         # Read the line that has the Owner's name(s)
-#         ownerNames=lines[257].strip()
-#         # Close and delete the file
-#         filein.close()
-#         try:
-#             os.remove(filename)
-#         except OSError:
-#             pass
-#         # Make that name more legible and replace semicolons with double commas
-#         ownerNamesPretty=ownerNames.replace('&amp;','&').replace('<br> ','').replace(';',',,')
-#         # Append the result to a CSV of parcel numbers and owner names
-#         # separated by semicolons
-#         csvOut = open(csvfilename,'a')
-#         csvOut.write("{};{}\n".format(parcelAndAddress,ownerNamesPretty))
-#         csvOut.close()
-#         # Impose a brief pause as a courtesy to the city
-#         sleep(4)
+        # Save that content to a local file
+        filename = "Madison_Parcel_{}.html".format(parcelNum)
+        fileout = open(filename,'wb')
+        fileout.write(resp.content)
+        fileout.close()
+        # Now open that file for reading
+        filein = open(filename,'r')
+        lines = filein.readlines()
+        # Read the line that has the Owner's name(s)
+        ownerNames=lines[257].strip()
+        # Close and delete the file
+        filein.close()
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+        # Make that name more legible and replace semicolons with double commas
+        ownerNamesPretty=ownerNames.replace('&amp;','&').replace('<br> ','').replace(';',',,')
+        # Append the result to a CSV of parcel numbers and owner names
+        # separated by semicolons
+        csvOut = open(csvfilename,'a')
+        csvOut.write("{};{}\n".format(parcelAndAddress,ownerNamesPretty))
+        csvOut.close()
+        # Impose a brief pause as a courtesy to the city
+        sleep(4)
 
 
 lastCSVEntryRE=re.compile("[^;]+$")
